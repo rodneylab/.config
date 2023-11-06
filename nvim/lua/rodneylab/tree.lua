6e1938bcac3d54b5e92768381800d79013c95e60
Rodney Lab <codesign@rodneylab.com>
2023-11-06 18:58:37 +0000

    fix: 💫 add NVimTree config

---
+++ nvim/lua/rodneylab/tree.lua
@@ -0,0 +1,24 @@
+--vim.g.loaded_netrw = 1
+--vim.g.loaded_netrwPlugin = 1
+
+vim.opt.termguicolors = true
+
+require("nvim-tree").setup({
+	sort_by = "case_sensitive",
+	renderer = {
+		group_empty = true,
+        root_folder_label = false,
+	},
+	filters = {
+		dotfiles = true,
+	},
+    disable_netrw = true,
+    hijack_netrw = true,
+    view = {
+        adaptive_size = false,
+        side = "left",
+        --width = 30,
+        width = 22,
+    }
+})
+
