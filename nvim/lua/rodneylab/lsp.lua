00f57b8a3fd47e3b83b089db2ff74eb97831155c
Rodney Lab <codesign@rodneylab.com>
2023-11-06 18:52:32 +0000

    fix: 💫 use Ruff LSP for Python

--- nvim/lua/rodneylab/lsp.lua
+++ nvim/lua/rodneylab/lsp.lua
@@ -26,7 +26,7 @@ end
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
-local servers = { 'astro','clangd', 'cssls', 'graphql', 'pylsp', 'rust_analyzer', 'svelte', 'taplo' }
+local servers = { 'astro', 'clangd', 'cssls', 'graphql', 'ruff_lsp', 'rust_analyzer', 'svelte', 'taplo' }
 for _, lsp in ipairs(servers) do
     nvim_lsp[lsp].setup  {
         capabilities = capabilities,
