6ea849436375cc630b58eae55d158d16d5d46853
Rodney Lab <codesign@rodneylab.com>
2024-11-21 13:53:24 +0000

    style: 🛁 linting

--- nvim/lua/rodneylab/tree.lua
+++ nvim/lua/rodneylab/tree.lua
@@ -1,25 +1,31 @@
---vim.g.loaded_netrw = 1
---vim.g.loaded_netrwPlugin = 1
-
 require("nvim-tree").setup({
 	sort_by = "case_sensitive",
 	renderer = {
 		group_empty = true,
-        root_folder_label = false,
+		root_folder_label = false,
 	},
 	filters = {
-		dotfiles = true,
+		dotfiles = false,
+		custom = { ".DS_Store", "node_modules", "target" },
 	},
-    disable_netrw = true,
-    hijack_netrw = true,
-    view = {
-        adaptive_size = false,
-        float = {
-            enable = true
-        },
-        side = "right",
-        --width = 30,
-        width = 22,
-    }
+	git = {
+		ignore = false,
+	},
+	disable_netrw = true,
+	hijack_netrw = true,
+	view = {
+		adaptive_size = false,
+		float = {
+			enable = true,
+		},
+		side = "right",
+		width = 22,
+	},
 })
 
+local keymap = vim.keymap
+
+keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle tree explorer" })
+keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle find file tree explorer" })
+keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file tree explorer" })
+keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file tree explorer" })
