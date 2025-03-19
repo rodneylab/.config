c22ca2a3c802cfb73b1bfd9c0cf996f1efe9ff37
Rodney Lab <codesign@rodneylab.com>
2025-03-19 16:14:58 +0000

    fix: 💫 update Rust crates config

--- nvim/lua/rodneylab/plugins/crates.lua
+++ nvim/lua/rodneylab/plugins/crates.lua
@@ -1,5 +1,17 @@
-require("crates").setup({
+local crates = require("crates")
+crates.setup({
 	completion = {
 		cmp = { enabled = true },
 	},
 })
+
+local keymap = vim.keymap
+
+keymap.set("n", "<leader>ct", crates.toggle, { desc = "Toggle crates" })
+keymap.set("n", "<leader>cr", crates.reload, { desc = "Reload crates" })
+
+keymap.set("n", "<leader>cv", crates.show_versions_popup, { desc = "Show crate versions" })
+keymap.set("n", "<leader>cf", crates.show_features_popup, { desc = "Show crate features" })
+keymap.set("n", "<leader>cd", crates.show_dependencies_popup, { desc = "Show crate dependencies" })
+
+keymap.set("n", "<leader>cu", crates.update_crate, { desc = "Update crate" })
