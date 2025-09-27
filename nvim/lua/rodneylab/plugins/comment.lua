ce3635c087a80f3e8ed7b239a7bdfdd088388791
Rodney Lab <codesign@rodneylab.com>
2025-09-27 15:02:12 +0100

    fix: 💫 update comment config

--- nvim/lua/rodneylab/plugins/comment.lua
+++ nvim/lua/rodneylab/plugins/comment.lua
@@ -4,3 +4,6 @@ local ts_context_commentstring = require("ts_context_commentstring.integrations.
 comment.setup({
 	pre_hook = ts_context_commentstring.create_pre_hook(),
 })
+
+local ft = require("Comment.ft")
+ft.set("dotenv", "#%s")
