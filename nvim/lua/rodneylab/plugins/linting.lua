1881d65faa777cd57a740ef73519b0fe1ee19815
Rodney Lab <codesign@rodneylab.com>
2025-12-30 11:21:09 +0000

    fix: 💫 update linting

--- nvim/lua/rodneylab/plugins/linting.lua
+++ nvim/lua/rodneylab/plugins/linting.lua
@@ -1,29 +1,29 @@
 local lint = require("lint")
 
 lint.linters_by_ft = {
-    astro = { "eslint_d" },
-    cpp = { "clangtidy" },
-    css = { "stylelint" },
-    dockerfile = { "hadolint" },
-    elixir = { "credo" },
-    javascript = { "eslint_d" },
-    javascriptreact = { "eslint_d" },
-    prisma = { "prisma-lint" },
-    python = { "ruff" },
-    scss = { "stylelint" },
-    sh = { "shellcheck" },
-    sql = { "sqruff" },
-    svelte = { "eslint_d" },
-    typescript = { "eslint_d" },
-    typescriptreact = { "eslint_d" },
+	astro = { "eslint_d" },
+	cpp = { "clangtidy" },
+	css = { "stylelint" },
+	dockerfile = { "hadolint" },
+	elixir = { "credo" },
+	javascript = { "eslint_d" },
+	javascriptreact = { "eslint_d" },
+	prisma = { "prisma-lint" },
+	python = { "ruff" },
+	scss = { "stylelint" },
+	sh = { "shellcheck" },
+	sql = { "sqruff" },
+	svelte = { "eslint_d" },
+	typescript = { "eslint_d" },
+	typescriptreact = { "eslint_d" },
 }
 
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-    callback = function()
-        lint.try_lint()
-    end,
+	callback = function()
+		lint.try_lint()
+	end,
 })
 
 vim.keymap.set("n", "<leader>l", function()
-    lint.try_lint()
+	lint.try_lint()
 end, { desc = "Trigger linting for current file" })
