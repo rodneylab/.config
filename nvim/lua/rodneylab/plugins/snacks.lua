80dcefc204f814242b899ec9a5348c80604f79b3
Rodney Lab <codesign@rodneylab.com>
2026-01-03 08:54:31 +0000

    fix: 💫 update nvim config

---
+++ nvim/lua/rodneylab/plugins/snacks.lua
@@ -0,0 +1,7 @@
+local snacks = require("snacks")
+snacks.setup({
+	input = { enabled = true },
+	notifier = { enabled = true },
+	picker = { enabled = true },
+	statuscolumn = { enabled = true },
+})
