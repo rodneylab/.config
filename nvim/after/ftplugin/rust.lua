b93898cd2f05852a3718e886967d39a1df5162c6
Rodney Lab <codesign@rodneylab.com>
2026-03-03 08:22:14 +0000

    fix: 💫 update rust nvim config

--- nvim/after/ftplugin/rust.lua
+++ nvim/after/ftplugin/rust.lua
@@ -1,7 +1,10 @@
 local bufnr = vim.api.nvim_get_current_buf()
 vim.keymap.set("n", "<leader>a", function()
-	vim.cmd.RustLsp("codeAction")
+    vim.cmd.RustLsp("codeAction")
 end, { silent = true, buffer = bufnr })
 vim.keymap.set("n", "<space>ca", function()
-	vim.cmd.RustLsp("codeAction")
+    vim.cmd.RustLsp("codeAction")
 end, { silent = true, buffer = bufnr })
+local opts = { noremap = true, silent = true }
+opts.desc = "Show LSP definitions"
+vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
