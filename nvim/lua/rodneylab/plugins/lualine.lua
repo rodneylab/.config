ea1e17228624b84104e0d987bdb5655f32592de1
Rodney Lab <codesign@rodneylab.com>
2026-04-08 04:46:52 +0100

    fix: 💫 update nvim lualine customisation

--- nvim/lua/rodneylab/plugins/lualine.lua
+++ nvim/lua/rodneylab/plugins/lualine.lua
@@ -9,15 +9,7 @@ end
 
 if vim.g.neovide then
 	-- override built-in catpuccin theme
-	local C = require("catppuccin.palettes").get_palette("mocha")
-	local custom_catppucin = require("lualine.themes.catppuccin")
-	custom_catppucin.command.a.bg = C.text
-	custom_catppucin.command.b.fg = C.text
-
 	require("lualine").setup({
-		options = {
-			theme = custom_catppucin,
-		},
 		sections = {
 			lualine_c = { "filename", { treelocation } },
 		},
