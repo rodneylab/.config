e4bdc18a6464405212ed7dbe75a8ee389d0ab5c2
Rodney Lab <codesign@rodneylab.com>
2026-04-08 05:29:16 +0100

    fix: 💫 update nvim lualine customisation

--- nvim/lua/rodneylab/plugins/catppuccin.lua
+++ nvim/lua/rodneylab/plugins/catppuccin.lua
@@ -16,4 +16,15 @@ require("catppuccin").setup({
 		nvimtree = true,
 		treesitter = true,
 	},
+	lualine = {
+		all = function(colors)
+			---@type CtpIntegrationLualineOverride
+			return {
+				command = {
+					a = { bg = colors.text },
+					b = { fg = colors.text },
+				},
+			}
+		end,
+	},
 })
